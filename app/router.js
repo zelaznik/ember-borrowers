e7ae7000d73f5fa6a4629efab7747d45d9f3d102
Steve Zelaznik <zelaznik@yahoo.com>
2016-08-05 17:50:38 -0500

    Made the routing look nicer.

--- app/router.js
+++ app/router.js
@@ -7,34 +7,21 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('friends', function() {
-    this.route('new');
+    this.route('friends', function() {
+      this.route('new');
 
-    this.route('show', {
-      path: ':friend_id'
-    }, function() {
-      this.route('loans', {resetNamespace: true}, function() {
+      this.route('show', {path: ':friend_id'}, function() {
+        this.route('loans', {resetNamespace: true}, function() {});
       });
+
+      this.route('edit', {path: ':friend_id/edit'});
     });
 
-    this.route('edit', {
-      path: ':friend_id/edit'
+    this.route('articles', function() {
+      this.route('new');
+      this.route('show', {path: ':article_id'});
+      this.route('edit', {path: ':article_id/edit'});
     });
-
-  });
-
-  this.route('articles', function() {
-    this.route('new');
-
-    this.route('show', {
-      path: ':article_id'
-    });
-
-    this.route('edit', {
-      path: ':article_id/edit'
-    });
-  });
-  this.route('loans');
 });
 
 export default Router;
